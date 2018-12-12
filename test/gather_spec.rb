cd9ae0a547db756c792dead28d6bb5ddfe42dba0
Вадим Тузов <v.tuzov@fun-box.ru>
2018-12-12 18:33:53 +0300

    run nithmethere.js with rspec

---
+++ test/gather_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+
+describe "Behaviour" do
+  def make_file_basename(host, port, domain)
+    "#{domain}__#{host}__#{port}".downcase.gsub(".", "_")
+  end
+
+  tasks = ENV['tasks']
+  assessment = ENV['assessment']
+  tasks.shuffle.each do |host, port, domain|
+    it "should send request to #{host}#{domain}:#{port}" do
+
+        file_basename = make_file_basename(host, port, domain)
+        url = Aquatone::UrlMaker.make(host, port)
+        html_destination = File.join(assessmenth, "html", "#{file_basename}.html")
+        headers_destination = File.join(assessment, "headers", "#{file_basename}.txt")
+        screenshot_destination = File.join(assessmenth, "screenshots", "#{file_basename}.png")
+        visit = Aquatone::Browser.visit(url, domain, html_destination, headers_destination, screenshot_destination, :timeout => options[:timeout])
+
+
+
+        if visit['success']
+          output("#{green('Processed:')} #{Aquatone::UrlMaker.make(host, port)} (#{domain}) - #{visit['status']}\n")
+          @successful += 1
+        else
+          output("   #{red('Failed:')} #{Aquatone::UrlMaker.make(host, port)} (#{domain}) - #{visit['error']} #{visit['details']}\n")
+          @failed += 1
+        end
+
+
+        expect(visit['code']).to be_in (["404","403"])
+
+      end
+
+
+    end
+
+  end
